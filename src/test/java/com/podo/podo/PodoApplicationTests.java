07ec725ab4ad5a066f867caa9d7d7e103b5a1917
용용 <yyong@YYongs-Mac-mini-M4.local>
2025-11-26 23:27:28 +0900

    chore: init spring boot project

---
+++ src/test/java/com/podo/podo/PodoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.podo.podo;
+
+import org.junit.jupiter.api.Test;
+
+class PodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // TODO: 나중에 진짜 스프링 컨텍스트/통합테스트로 바꾸자
+    }
+
+}
